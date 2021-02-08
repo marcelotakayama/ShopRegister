01a84fbba0d593342bf9201770c15cdea8704299
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-08 17:55:21 -0300

    criando o controller

---
+++ ShopRegisterAPI/ShopRegisterAPI/Controllers/ProductDetailController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ShopRegisterAPI.Models;
+
+namespace ShopRegisterAPI.Controllers
+{
+    public class ProductDetailController : Controller
+    {
+        private readonly ProductDetailContext _context;
+
+        public ProductDetailController(ProductDetailContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ProductDetail
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ProductDetails.ToListAsync());
+        }
+
+        // GET: ProductDetail/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productDetail = await _context.ProductDetails
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (productDetail == null)
+            {
+                return NotFound();
+            }
+
+            return View(productDetail);
+        }
+
+        // GET: ProductDetail/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductDetail/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductId,ProductName,ProductCode,ProductRegisterDate,ProductPrice")] ProductDetail productDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(productDetail);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productDetail);
+        }
+
+        // GET: ProductDetail/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productDetail = await _context.ProductDetails.FindAsync(id);
+            if (productDetail == null)
+            {
+                return NotFound();
+            }
+            return View(productDetail);
+        }
+
+        // POST: ProductDetail/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,ProductName,ProductCode,ProductRegisterDate,ProductPrice")] ProductDetail productDetail)
+        {
+            if (id != productDetail.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(productDetail);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductDetailExists(productDetail.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productDetail);
+        }
+
+        // GET: ProductDetail/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productDetail = await _context.ProductDetails
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (productDetail == null)
+            {
+                return NotFound();
+            }
+
+            return View(productDetail);
+        }
+
+        // POST: ProductDetail/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var productDetail = await _context.ProductDetails.FindAsync(id);
+            _context.ProductDetails.Remove(productDetail);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductDetailExists(int id)
+        {
+            return _context.ProductDetails.Any(e => e.ProductId == id);
+        }
+    }
+}
