0b4af99b5b12014db22ec32fc602df4a2cab6fd7
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-08 17:18:01 -0300

    Configurando o contexto do banco de dados

---
+++ ShopRegisterAPI/ShopRegisterAPI/Models/ProductDetail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopRegisterAPI.Models {
+    public class ProductDetail {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Column(TypeName ="nvarchar(100)")]
+        public string ProductName { get; set; }
+
+        [Column(TypeName ="varchar=10")]
+        public string ProductCode { get; set; }
+
+        [Column(TypeName ="varchar=5")]
+        public string ProductRegisterDate { get; set; }
+
+        [Column(TypeName ="varchar=100000")]
+        public long ProductPrice { get; set; }
+    }
+}
---
+++ ShopRegisterAPI/ShopRegisterAPI/Models/ProductDetailContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopRegisterAPI.Models {
+    public class ProductDetailContext: DbContext {
+        public ProductDetailContext(DbContextOptions<ProductDetailContext> options) : base(options) {
+        }
+
+        public DbSet<ProductDetail> ProductDetails { get; set; }
+    }
+}
