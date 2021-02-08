8ef2572a07dd41da70e4ce40bb4954277e50457a
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-08 17:48:55 -0300

    FOI

---
+++ ShopRegisterAPI/ShopRegisterAPI/Migrations/20210208204836_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShopRegisterAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductDetails",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    ProductCode = table.Column<string>(type: "nvarchar(10)", nullable: true),
+                    ProductRegisterDate = table.Column<string>(type: "nvarchar(5)", nullable: true),
+                    ProductPrice = table.Column<string>(type: "nvarchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductDetails", x => x.ProductId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductDetails");
+        }
+    }
+}
